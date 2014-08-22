6320b4d5c61388af86ee0517b247d27c1f441f9b
joerong <joerong@localhost.localdomain>
2014-08-23 05:48:13 +0800

    improve prod_inc.h

--- prod_inc.h
+++ prod_inc.h
@@ -3,10 +3,26 @@
 
 #include "log.h"
 
-#define _malloc_  malloc
-#define _realloc_ realloc
-#define _free_    free
-#define _strdup_  strdup
-#define _strndup_ strndup
+#if 0
+/************************************************
+** define log functions if needed
+*************************************************/
+#  define log_trace   your_log_function 
+#  define log_debug   your_log_function
+#  define log_info    your_log_function
+#  define log_warn    your_log_function
+#  define log_error   your_log_function
+#  define log_fatal   your_log_function
+#  define log_prompt  your_log_function
+
+/************************************************
+** define memory functions if needed
+*************************************************/
+#  define _malloc_    malloc
+#  define _realloc_   realloc
+#  define _free_      free
+#  define _strdup_    strdup
+#  define _strndup_   strndup
+#endif
 
 #endif
